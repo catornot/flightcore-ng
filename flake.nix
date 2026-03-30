9fe62ec5939d4f31b45234fc8bc8561e887875b9
catornot <41955154+catornot@users.noreply.github.com>
2026-03-30 15:09:54 -0400

    initial commit

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      utils,
+      rust-overlay,
+    }:
+    utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ (import rust-overlay) ];
+        };
+        toolchain = (pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml);
+
+      in
+      {
+        formatter = pkgs.nixfmt-tree;
+
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            toolchain
+
+          ];
+        };
+      }
+    );
+}
